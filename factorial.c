f17333e8d130c841d92dfc35bdca21b324b92913
Abraham Rojas <dpenso.fsaeusb@gmail.com>
2022-01-22 19:04:37 -0400

    prueba

---
+++ factorial.c
@@ -0,0 +1,21 @@
+    /* Nombre del archivo:
+    /* Descriopcion del archivo:
+    /* Autor:
+    /* Ultima actualizacion:
+    */
+
+#include <stdio.h>
+    int main(){
+        int n,i, multi=0;
+
+        printf("Ingrese el un numero para realizar el factorial del mismo: ");
+        scanf("%i",&n);
+
+        if
+
+        }
+    printf("El factorial de %i es: %i",n,multi);
+
+
+    return 0;
+              }
